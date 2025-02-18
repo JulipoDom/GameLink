c65261fd37f077e7350c481f14ad4da30d113e17
Julio Domigos <jdnn2006@gmail.com>
2025-02-18 20:44:52 -0300

    I hate media screen querys

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -170,7 +170,7 @@ export default function Home() {
         </Card>
         <Card className="w-11/12 m-5 p-5 flex flex-col justify-center items-center">
           <Label className="text-lg">Meus jogos</Label>
-          <Card className="w-full m-5 grid grid-cols-5 gap-3 shadow-none border-0">
+          <Card className="w-full m-5 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-3 shadow-none border-0">
         {
           games ?
           games.map((game) => (
